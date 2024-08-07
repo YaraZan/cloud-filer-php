c6735916ecf539402f7494f646307377c337838a
andrei_potiomkin <89867134953@mail.ru>
2024-08-07 08:15:30 +0300

    feat(service): (#20) created FileServiceMeta class

---
+++ src/Services/Meta/FileServiceMeta.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Meta;
+
+interface FileServiceMeta {
+    public function getAll(): array;
+    public function get(int $id): object;
+    public function add($file): void;
+    public function rename(int $id, string $newname): void;
+    public function remove(int $id): void;
+}
+?>

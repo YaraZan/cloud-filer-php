af108f2146bec0144f85cdbafc2a8d48df133b5b
andrei_potiomkin <89867134953@mail.ru>
2024-08-07 08:06:02 +0300

    feat(service): (#20) created UserServiceMeta class

--- src/Services/Meta/UserServiceMeta.php
+++ src/Services/Meta/UserServiceMeta.php
@@ -2,8 +2,12 @@
 
 namespace App\Services\Meta;
 
-interface UserServiceMeta {
+interface UserServiceMeta
+{
     public function getAllUsers(): array;
     public function getUserById(int $id): object;
+    public function updateAuthorizedUser($data): bool;
+    public function login($credentials): bool;
+    public function logout(): void;
+    public function reset_password(string $old_password, string $new_password): bool;
 }
-?>

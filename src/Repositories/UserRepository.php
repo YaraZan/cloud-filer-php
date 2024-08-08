2a0492f2d81917f97324fdcedba38ecf9e877515
andrei_potiomkin <89867134953@mail.ru>
2024-08-08 10:06:00 +0300

    feat(repo): (#32) overrided methods for user repo

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -4,7 +4,23 @@ namespace App\Repositories;
 
 use App\Core\DB;
 
-class UserRepository extends DB {
+class UserRepository extends DB
+{
     protected string $tableName = "users";
+
+    public function findAll(): array
+    {
+        $sql = "SELECT name, email from " . $this->tableName;
+        $res = $this->executeQuery($sql);
+
+        return $res;
+    }
+
+    public function findOne(int $id): object
+    {
+        $sql = "SELECT name, email, bio from " . $this->tableName;
+        $res = $this->executeQuery($sql, [$id], false);
+
+        return $res;
+    }
 }
-?>

3dc58b6c8083d46ac95139e8d7dca3e0bf3d64a5
andrei_potiomkin <89867134953@mail.ru>
2024-08-12 09:01:06 +0300

    feat(core): (#50) created core Session class

--- src/Core/Session.php
+++ src/Core/Session.php
@@ -2,6 +2,58 @@
 
 namespace App\Core;
 
-class Session {
-    
+class Session
+{
+    public static function start(): void
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public static function create(array $user): void
+    {
+        self::start();
+        $_SESSION['uid'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        self::regenerate();
+    }
+
+    public static function get(string $key)
+    {
+        self::start();
+        return $_SESSION[$key] ?? null;
+    }
+
+    public static function set(string $key, $value): void
+    {
+        self::start();
+        $_SESSION[$key] = $value;
+    }
+
+    public static function destroy(): void
+    {
+        self::start();
+        session_unset();
+        session_destroy();
+    }
+
+    public static function isAuthorized(): bool
+    {
+        self::start();
+        return isset($_SESSION['uid']);
+    }
+
+    private static function regenerate(): void
+    {
+        if (session_status() == PHP_SESSION_ACTIVE) {
+            session_regenerate_id(true);
+        }
+    }
+
+    public static function unset(string $key): void
+    {
+        self::start();
+        unset($_SESSION[$key]);
+    }
 }

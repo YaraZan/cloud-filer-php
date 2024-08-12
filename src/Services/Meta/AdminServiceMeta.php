cab645054106cb1247c90bc3bca46f17461d70eb
andrei_potiomkin <89867134953@mail.ru>
2024-08-12 07:59:36 +0300

    refactor: (#51) removed unnecessary PHP tag clauses

--- src/Services/Meta/AdminServiceMeta.php
+++ src/Services/Meta/AdminServiceMeta.php
@@ -2,10 +2,10 @@
 
 namespace App\Services\Meta;
 
-interface AdminServiceMeta {
+interface AdminServiceMeta
+{
     public function getAllUsers(): array;
     public function getUser(int $id): object;
     public function deleteUser(int $id): void;
     public function updateUser(int $id, $data): void;
 }
-?>

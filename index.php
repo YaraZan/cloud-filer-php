bca4295175527dc86b282c43507065eb2ff40777
andrei_potiomkin <89867134953@mail.ru>
2024-08-15 08:35:57 +0300

    feat: (#63) added get login view, added request instance in index.php

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
 
+use App\Controllers\AuthController;
+use App\Core\Request;
 use App\Core\Response;
 
 require_once 'vendor/autoload.php';
 
 $routes = [
-    // routes
+    "GET /login" => [AuthController::class, "view"]
 ];
 
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
+$data = $method === 'POST' ? $_POST : $_GET;
+
+$request = new Request($data, $uri, $method);
 
 $routeKey = $method . ' ' . strtok($uri, '?');
 if (isset($routes[$routeKey])) {
     [$controller, $method] = $routes[$routeKey];
     $controllerInstance = new $controller();
-    $response = $controllerInstance->$method($_REQUEST);
+    $response = $controllerInstance->$method($request);
 
     if ($response instanceof Response) {
         $response->send();

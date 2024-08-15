47d580e2162d7c4235f9805f777f08d56edde657
andrei_potiomkin <89867134953@mail.ru>
2024-08-15 08:25:00 +0300

    feat(controller): (#63) add user service instance, created view() method

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controllers;
 
+use App\Core\App;
+use App\Core\Request;
 use App\Core\Response;
+use App\Services\Impl\UserService;
 
 class AuthController
 {
+    private UserService $userService;
+
+    public function __construct() {
+        $this->userService = App::getService('userService');
+    }
+
     public function view(): Response
     {
-        
+        return new Response("login.php", [], 200);
     }
 }

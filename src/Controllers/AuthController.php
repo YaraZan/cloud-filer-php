d84215677c526a500033fa9cbe3261fab3b5bf98
andrei_potiomkin <89867134953@mail.ru>
2024-08-20 09:52:03 +0300

    hotfix(controller)!: (#74) change register method in AuthController

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -3,20 +3,19 @@
 namespace App\Controllers;
 
 use App\Core\App;
+use App\Core\Controller;
 use App\Core\Request;
 use App\Core\Response;
 use App\Services\Impl\UserService;
 
-class AuthController
+class AuthController extends Controller
 {
-    private UserService $userService;
-
-    public function __construct() {
-        $this->userService = App::getService('userService');
-    }
-
-    public function view(): Response
+    public function register(Request $request): Response
     {
-        return new Response("login.php", [], 200);
+        $userService = App::getService('userService');
+        
+        $userService->register($request->getData());
+
+        return new Response(["message" => "Registration successfull!"], 200);
     }
 }

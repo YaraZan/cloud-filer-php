c3d407f84448abf07e141333d79bac18f0953488
andrei_potiomkin <89867134953@mail.ru>
2024-08-21 10:30:55 +0300

    hotfix(service): (#74) change login() method return type to string

--- src/Services/Meta/UserServiceMeta.php
+++ src/Services/Meta/UserServiceMeta.php
@@ -7,7 +7,7 @@ interface UserServiceMeta
     public function getAllUsers(): array;
     public function getUser(int $id): array;
     public function updateAuthorizedUser($data): void;
-    public function login($credentials): void;
+    public function login($credentials): string;
     public function register($credentials): void;
     public function logout(): void;
     public function resetPassword(string $old_password, string $new_password, string $confirm_password): void;

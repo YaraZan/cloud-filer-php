fea10f36b34fd5f9b65f780af9317989fa37473d
andrei_potiomkin <89867134953@mail.ru>
2024-08-22 15:04:02 +0300

    feat(core): (#77) wrote docs for Response core class

--- src/Core/Response.php
+++ src/Core/Response.php
@@ -2,10 +2,18 @@
 
 namespace App\Core;
 
+/**
+ * Represents server response
+ */
 class Response
 {
+    /** Response data */
     private $data;
+
+    /** Response status code */
     private int $statusCode = 200;
+
+    /** Response headers */
     private array $headers = [];
 
     public function __construct($data, int $statusCode = 200, array $headers = [])
@@ -15,36 +23,74 @@ class Response
         $this->headers = $headers;
     }
 
+    /**
+     * Set payload to response
+     * 
+     * @param mixed $data Response data
+     * @return void
+     */
     public function setData($data): void
     {
         $this->data = $data;
     }
 
-    public function getData()
+    /**
+     * Get response payload
+     * 
+     * @return mixed Response data
+     */
+    public function getData(): mixed
     {
         return $this->data;
     }
 
+    /**
+     * Set headers to response
+     * 
+     * @param array $headers Response headers
+     * @return void
+     */
     public function setHeaders(array $headers): void
     {
         $this->headers = $headers;
     }
 
+    /**
+     * Get response headers
+     * 
+     * @return array Responce headers
+     */
     public function getHeaders(): array
     {
         return $this->headers;
     }
 
+    /**
+     * Get response status code
+     * 
+     * @return int Responce status code
+     */
     public function getStatusCode(): int
     {
         return $this->statusCode;
     }
 
+    /**
+     * Set response status code
+     * 
+     * @param int $statusCode Responce status code
+     * @return void
+     */
     public function setStatusCode(int $statusCode): void
     {
         $this->statusCode = $statusCode;
     }
 
+    /**
+     * Send response
+     * 
+     * @return void
+     */
     public function send(): void
     {
         http_response_code($this->statusCode);

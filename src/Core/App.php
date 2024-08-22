41b73829d2e73d8ff98b5b19c9ded43c53a9a6c2
andrei_potiomkin <89867134953@mail.ru>
2024-08-22 14:08:09 +0300

    feat(core): (#77) wrote docs for App core class

--- src/Core/App.php
+++ src/Core/App.php
@@ -4,16 +4,28 @@ namespace App\Core;
 
 use Exception;
 
+/**
+ * A service resgistry class.
+ */
 class App
 {
+    /** Array of registered services */
     private static array $services;
 
+    /**
+     * Call to this function registrates all defined services.
+     * @return void
+     */
     public static function registerServices(): void
     {
         self::$services['userService'] = new \App\Services\Impl\UserService();
     }
 
-    public static function getService(string $name)
+    /**
+     * Returns service instance by it's name.
+     * @return mixed
+     */
+    public static function getService(string $name): mixed
     {
         if (!isset(self::$services[$name])) {
             throw new Exception("Service not found: " . $name);

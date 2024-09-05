1c2b41836185ddbc6b693e2fbe9d2f5838fbbd8f
andrei_potiomkin <89867134953@mail.ru>
2024-09-05 10:36:27 +0300

    refactor(middleware)!: (#76) change auth logic to cookies

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -20,24 +20,14 @@ class AuthMiddleware extends Middleware
     public function handle(Request $request): void
     {
         // Get request headers
-        $headers = $request->getHeaders();
+        $cookies = $request->getCookies();
 
         // Check if there is no authorization schema
-        if (!isset($headers["Authorization"])) {
-            throw new Exception("No authorization schema provided!", 401);
+        if (!isset($cookies['token'])) {
+            throw new Exception("No token provided!", 401);
         }
 
-        // Desctructurize schema and token
-        [$schema, $token] = explode(" ", $headers["Authorization"]); 
-
-        // Check if used schema is valid
-        if ($schema !== "Bearer") {
-            throw new Exception("Invalid authorization schema", 401);
-        }
-        // Check if token provided
-        if (!isset($token)) {
-            throw new Exception("Token invalid", 401);
-        }
+        $token = $cookies['token'];
 
         $this->userService->authorize($token);
     }

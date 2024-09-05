32fc048eb0783e3e46f2cb30cdc32b779764f5e5
andrei_potiomkin <89867134953@mail.ru>
2024-09-05 08:07:02 +0300

    feat(utils): (#76) add createAccessToken and createRefreshToken methods to Tokenizer

--- src/Utils/Tokenizer.php
+++ src/Utils/Tokenizer.php
@@ -18,4 +18,25 @@ class Tokenizer
     {
         return JWT::decode($jwt, new Key(JWT_SECRET, 'HS256'));
     }
+
+    public static function createAccessToken(array $user): string
+    {
+        $accessToken = [
+            "exp" => round(microtime(true) * 1000) + (ACCESS_TOKEN_EXP),
+            "iat" => round(microtime(true)),
+            "did" => hash('sha256', $_SERVER["HTTP_USER_AGENT"] . $_SERVER["REMOTE_ADDR"]),
+            "user" => $user,
+        ];
+        return self::encode($accessToken);
+    }
+
+    public static function createRefreshToken(array $user): string
+    {
+        $refreshToken = [
+            "exp" => round(microtime(true) * 1000) + (REFRESH_TOKEN_EXP),
+            "iat" => round(microtime(true)),
+            "uid" => $user["id"],
+        ];
+        return self::encode($refreshToken);
+    }
 }

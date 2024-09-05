8af553cbb8b30f16663c7d4c5c6c461b691c46ea
andrei_potiomkin <89867134953@mail.ru>
2024-09-05 08:27:39 +0300

    refactor(middleware)!: (#76) move db and token related logic to UserService class

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -5,32 +5,40 @@ namespace App\Middleware;
 use App\Core\Middleware;
 use App\Core\Request;
 use App\Core\Session;
-use App\Exceptions\TokenExpiredException;
-use App\Exceptions\TokenInvalidException;
+use App\Services\Impl\UserService;
 use App\Utils\Tokenizer;
 use Exception;
 
 class AuthMiddleware extends Middleware
 {
+    private UserService $userService;
+
+    public function __construct() {
+        $this->userService = new UserService();
+    }
+
     public function handle(Request $request): void
     {
-        $data = $request->getData();
+        // Get request headers
+        $headers = $request->getHeaders();
 
-        $storedToken = Session::get("token");
-        $receivedToken = $data["token"] ?? null;
+        // Check if there is no authorization schema
+        if (!isset($headers["Authorization"])) {
+            throw new Exception("No authorization schema provided!", 401);
+        }
 
-        if (!isset($receivedToken)) {
+        // Desctructurize schema and token
+        [$schema, $token] = explode(" ", $headers["Authorization"]); 
+
+        // Check if used schema is valid
+        if ($schema !== "Bearer") {
+            throw new Exception("Invalid authorization schema", 401);
+        }
+        // Check if token provided
+        if (!isset($token)) {
             throw new Exception("Token invalid", 401);
         }
 
-        if ($storedToken !== $receivedToken) {
-            throw new Exception("Token invalid", 401);
-        }
-
-        $decodedToken = Tokenizer::decode($storedToken);
-
-        if ($decodedToken->exp < time()) {
-            throw new Exception("Token expired", 419);
-        }
+        $this->userService->authorize($token);
     }
 }

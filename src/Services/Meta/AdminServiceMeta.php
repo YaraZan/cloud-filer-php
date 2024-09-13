64592cb72d1a3ea508f8c852dc503481c8cb2630
andrei_potiomkin <89867134953@mail.ru>
2024-09-13 14:52:06 +0300

    hotfix(service)!: (#82) change methods return types from object to array

--- src/Services/Meta/AdminServiceMeta.php
+++ src/Services/Meta/AdminServiceMeta.php
@@ -5,7 +5,7 @@ namespace App\Services\Meta;
 interface AdminServiceMeta
 {
     public function getAllUsers(): array;
-    public function getUser(int $id): object;
+    public function getUser(int $id): array;
     public function deleteUser(int $id): void;
     public function updateUser(int $id, $data): void;
 }

daece0e500b600000cd21cdcb0239026d43ade34
andrei_potiomkin <89867134953@mail.ru>
2024-09-20 13:33:12 +0300

    feat(exception): (#83) add ValidatorExceptions class with Validator utility errors

---
+++ src/Exceptions/ValidatorExceptions.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Exceptions;
+
+/**
+ * Abstract class to determine user exceptions.
+ */
+abstract class ValidatorException extends BaseException {};
+
+/**
+ * Property exceptions
+ */
+class ValidatorPropertyException extends ValidatorException
+{
+    public static function isRequired(string $propertyName): self
+    {
+        return self::errorBadArguments(
+            sprintf(
+                "The '%s' property is required!",
+                $propertyName
+            )
+        );
+    }
+
+    public static function lenghtOverLimit(string $propertyName): self
+    {
+        return self::errorBadArguments(
+            sprintf(
+                "The '%s' property's length is over it's maximum limit!",
+                $propertyName
+            )
+        );
+    }
+
+    public static function lenghtUnderLimit(string $propertyName): self
+    {
+        return self::errorBadArguments(
+            sprintf(
+                "The '%s' property's length is under it's minimum limit!",
+                $propertyName
+            )
+        );
+    }
+}

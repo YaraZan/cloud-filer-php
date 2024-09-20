cdc04bc58297121dc721ffdff83f331dceb3f195
andrei_potiomkin <89867134953@mail.ru>
2024-09-20 08:47:37 +0300

    feat(factories): (#83) create UserFactory class

---
+++ src/Factories/UserFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Factories;
+
+use App\Core\Factory;
+
+class UserFactory extends Factory
+{
+    public function work(int $numRecords): void
+    {
+        $this->seed($numRecords, [
+            "name" => fn() => $this->faker->name(),
+            "email" => fn() => $this->faker->email(),
+            "password" => fn() => $this->faker->password()
+        ]);
+    }
+}

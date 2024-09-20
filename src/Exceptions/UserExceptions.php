3e400962d44c378f23ce79ac9c9a671c65b46777
andrei_potiomkin <89867134953@mail.ru>
2024-09-20 13:31:41 +0300

    feat(exception): (#83) add UserException class with UserService related errors

---
+++ src/Exceptions/UserExceptions.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Exceptions;
+
+/**
+ * Abstract class to determine user exceptions.
+ */
+abstract class UserException extends BaseException {};
+
+/**
+ * Email exceptions.
+ */
+class EmailException extends UserException {
+    public static function invalidFormat(): self 
+    {
+        return self::errorBadArguments("Invalid email format. Email should match pattern: example@mail.com",);
+    }
+
+    public static function alreadyExists(): self 
+    {
+        return self::errorBadArguments("User with provided email already exists",);
+    }
+
+    public static function doesntExists(): self 
+    {
+        return self::errorBadArguments("User with provided email doesn`t exist",);
+    }
+}; 
+
+/**
+ * Password exceptions.
+ */
+class PasswordException extends UserException {
+    public static function invalidFormat(): self
+    {
+        return self::errorBadArguments("Invalid password format. Password should be 12 digits 
+    lenght, including numbers and spec. symbols: /, #, $, %, *, _, -");
+    }
+
+    public static function incorrect(): self
+    {
+        return self::errorBadArguments("Password incorrect");
+    }
+
+    public static function doesntMatch(): self
+    {
+        return self::errorBadArguments("Passwords doesnt match");
+    }
+}
+
+/**
+ * Token exceptionxs.
+ */
+class TokenException extends UserException
+{
+    public static function doesntExist(): self
+    {
+        return self::errorUnauthorized("Token doesn't exist!");
+    }
+
+    public static function invalid(): self
+    {
+        return self::errorUnauthorized("Token invalid");
+    }
+}

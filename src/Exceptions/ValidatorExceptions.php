5c9b9d88ee78c2c4808aaf2758f945109206647e
andrei_potiomkin <89867134953@mail.ru>
2024-09-25 15:37:31 +0300

    feat(exceptions): (#83) add ValidatorExceptions class with validator exceptions

--- src/Exceptions/ValidatorExceptions.php
+++ src/Exceptions/ValidatorExceptions.php
@@ -12,33 +12,33 @@ abstract class ValidatorException extends BaseException {};
  */
 class ValidatorPropertyException extends ValidatorException
 {
-    public static function isRequired(string $propertyName): self
-    {
-        return self::errorBadArguments(
-            sprintf(
-                "The '%s' property is required!",
-                $propertyName
-            )
-        );
-    }
+  public static function isRequired(string $propertyName): self
+  {
+    return self::errorBadArguments(
+      sprintf(
+        "The '%s' property is required!",
+        $propertyName
+      )
+    );
+  }
 
-    public static function lenghtOverLimit(string $propertyName): self
-    {
-        return self::errorBadArguments(
-            sprintf(
-                "The '%s' property's length is over it's maximum limit!",
-                $propertyName
-            )
-        );
-    }
+  public static function lenghtOverLimit(string $propertyName): self
+  {
+    return self::errorBadArguments(
+      sprintf(
+        "The '%s' property's length is over it's maximum limit!",
+        $propertyName
+      )
+    );
+  }
 
-    public static function lenghtUnderLimit(string $propertyName): self
-    {
-        return self::errorBadArguments(
-            sprintf(
-                "The '%s' property's length is under it's minimum limit!",
-                $propertyName
-            )
-        );
-    }
+  public static function lenghtUnderLimit(string $propertyName): self
+  {
+    return self::errorBadArguments(
+      sprintf(
+        "The '%s' property's length is under it's minimum limit!",
+        $propertyName
+      )
+    );
+  }
 }

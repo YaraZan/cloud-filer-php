869318a91031c22a9ad47c14a506a0d4a58740bc
andrei_potiomkin <89867134953@mail.ru>
2024-09-25 13:16:20 +0300

    hotfix(exceptions): (#83) change BaseException internal error with default text message

--- src/Exceptions/BaseException.php
+++ src/Exceptions/BaseException.php
@@ -10,45 +10,45 @@ use Exception;
 class BaseException extends Exception {
 
     // 404 Not Found error
-    protected static function errorNotFound(string $message): self
+    protected static function errorNotFound(string $message): static
     {
-        return new self($message, 404);
+        return new static($message, 404);
     }
 
     // 403 authentication error (Unauthenticated Access)
-    protected static function errorUnauthenticated(): self
+    protected static function errorUnauthenticated(): static
     {
-        return new self("Not authenticated", 401);
+        return new static("Not authenticated", 401);
     }
 
     // 403 Forbidden error (Unauthorized Access)
-    protected static function errorUnauthorized(): self
+    protected static function errorUnauthorized(): static
     {
-        return new self("Not authorized", 403);
+        return new static("Not authorized", 403);
     }
 
     // 409 Conflict error (Duplicate entry or conflict)
-    protected static function errorConflict(string $message): self
+    protected static function errorConflict(string $message): static
     {
-        return new self($message, 409);
+        return new static($message, 409);
     }
 
     // 500 Internal Server error (Generic server failure)
-    protected static function errorInternal(): self
+    protected static function errorInternal(): static
     {
-        return new self("Server currently unavailable or cannot
+        return new static("Server currently unavailable or cannot
         process this action. Please try it later.", 500);
     }
 
     // 400 Bad Request error (Bad data or invalid arguments)
-    protected static function errorBadArguments(string $message): self
+    protected static function errorBadArguments(string $message): static
     {
-        return new self($message, 400);
+        return new static($message, 400);
     }
 
     // 419 Session Expired error
-    protected static function errorExpired(string $message): self
+    protected static function errorExpired(string $message): static
     {
-        return new self($message, 419);
+        return new static($message, 419);
     }
 }

595e5a4f6d8bcb3003b2d25cd93409523a4ab31c
andrei_potiomkin <89867134953@mail.ru>
2024-09-25 15:36:40 +0300

    hotfix(exceptions)!: (#83) move email and password exceptions to AuthExceptions class

--- src/Exceptions/AuthExceptions.php
+++ src/Exceptions/AuthExceptions.php
@@ -55,3 +55,43 @@ class SessionException extends AuthException
     return self::errorUnauthenticated();
   }
 }
+
+class EmailException extends AuthException
+{
+  public static function invalidFormat(): self
+  {
+    return self::errorBadArguments("Invalid email format. Email should match pattern: example@mail.com",);
+  }
+
+  public static function alreadyExists(): self
+  {
+    return self::errorBadArguments("User with provided email already exists",);
+  }
+
+  public static function doesntExists(): self
+  {
+    return self::errorBadArguments("User with provided email doesn`t exist",);
+  }
+};
+
+/**
+ * Password exceptions.
+ */
+class PasswordException extends AuthException
+{
+  public static function invalidFormat(): self
+  {
+    return self::errorBadArguments("Invalid password format. Password should be from 8 to 20 digits
+  lenght, including numbers and spec. symbols: /, #, $, %, *, _, -");
+  }
+
+  public static function incorrect(): self
+  {
+    return self::errorBadArguments("Password incorrect");
+  }
+
+  public static function doesntMatch(): self
+  {
+    return self::errorBadArguments("Passwords doesnt match");
+  }
+}

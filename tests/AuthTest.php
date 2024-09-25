0bbe198b2d7c0bde3b35b3b6e50fa5cc4eb0795a
andrei_potiomkin <89867134953@mail.ru>
2024-09-25 14:58:30 +0300

    feat(tests): (#83) add tests for AuthSerice class in AuthTests, also including Validator utility

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests;
 
-require_once __DIR__ . '/../src/Exceptions/UserExceptions.php';
 require_once __DIR__ . '/../src/Exceptions/AuthExceptions.php';
 require_once __DIR__ . '/../src/Exceptions/ValidatorExceptions.php';
 
@@ -15,10 +14,127 @@ use PHPUnit\Framework\TestCase;
 
 class AuthTest extends TestCase
 {
+  private UserRepository $userRepository;
+  private AuthService $authService;
+
+  protected function setUp(): void
+  {
+      parent::setUp();
+      $this->userRepository = new UserRepository();
+      $this->authService = new AuthService();
+  }
+
+  protected function tearDown(): void
+  {
+      parent::tearDown();
+
+      $this->userRepository->clearTable();
+  }
+
+  public function testUserCannotRegisterWithInvalidEmail(): void
+  {
+    $this->expectException(EmailException::class);
+
+    $invalidEmailUser = [
+        "name" => "John Doe",
+        "email" => "invalid-email",
+        "password" => "ValidPass123",
+        "confirm_password" => "ValidPass123"
+    ];
+
+    $this->authService->register($invalidEmailUser);
+  }
+
+  public function testUserCannotRegisterWithNameMoreThan255Digits(): void
+  {
+    $this->expectException(ValidatorException::class);
+
+    $existingUser = [
+        "name" => "Very l" . str_repeat("o", 255) . "ng name",
+        "email" => "johndoe@example.com",
+        "password" => "ValidPass123#%",
+        "confirm_password" => "ValidPass123#%"
+    ];
+
+    $this->authService->register($existingUser);
+  }
+
+  public function testUserCannotRegisterWithEmailMoreThan255Digits(): void
+  {
+    $this->expectException(ValidatorException::class);
+
+    $existingUser = [
+        "name" => "John Doe",
+        "email" => "Very l" . str_repeat("o", 255) . "ng email",
+        "password" => "ValidPass123#%",
+        "confirm_password" => "ValidPass123#%"
+    ];
+
+    $this->authService->register($existingUser);
+  }
+
+  public function testUserCannotRegisterWithPasswordMoreThan20Digits(): void
+  {
+    $this->expectException(ValidatorException::class);
+
+    $existingUser = [
+        "name" => "John Doe",
+        "email" => "johndoe@example.com",
+        "password" => "Very l" . str_repeat("o", 20) . "ng password#%",
+        "confirm_password" => "ValidPass123#%"
+    ];
+
+    $this->authService->register($existingUser);
+  }
+
+  public function testUserCannotRegisterWithPasswordLessThan8Digits(): void
+  {
+    $this->expectException(ValidatorException::class);
+
+    $existingUser = [
+        "name" => "John Doe",
+        "email" => "johndoe@example.com",
+        "password" => "Sm4ll#%",
+        "confirm_password" => "Sm4ll#%"
+    ];
+
+    $this->authService->register($existingUser);
+  }
+
+  public function testUserCannotRegisterIfPasswordsDoesntMatch(): void
+  {
+    $this->expectException(PasswordException::class);
+
+    $existingUser = [
+        "name" => "John Doe",
+        "email" => "johndoe@example.com",
+        "password" => "ValidPass123#%",
+        "confirm_password" => "ValidPass12#%"
+    ];
+
+    $this->authService->register($existingUser);
+  }
+
+  public function testUserCannotRegisterIfUserWithProvidedEmailAlreadyExists(): void
+  {
+    $this->expectException(EmailException::class);
+
+    $existingUser = [
+        "name" => "John Doe",
+        "email" => "johndoe@example.com",
+        "password" => "ValidPass123#%",
+        "confirm_password" => "ValidPass123#%"
+    ];
+
+    // Register user for the first time
+    $this->authService->register($existingUser);
+
+    // Try to register it for the second
+    $this->authService->register($existingUser);
+  }
+
   public function testUserCanRegisterWithValidPassword(): void
   {
-    $repository = new UserRepository();
-    $authService = new AuthService();
     $validUser = [
         "name" => "John Doe",
         "email" => "johndoe@example.com",
@@ -26,86 +142,10 @@ class AuthTest extends TestCase
         "confirm_password" => "ValidPass123#%"
     ];
 
-    $authService->register($validUser);
-    $this->assertTrue(true);
+    $this->authService->register($validUser);
 
-    $repository->clearTable();
-  }
+    $user = $this->userRepository->findOneWhere(sprintf("email = '%s'", $validUser["email"]));
 
-  public function testUserCannotRegisterWithInvalidPassword(): void
-  {
-    $this->expectException(PasswordException::class);
-
-    $repository = new UserRepository();
-    $authService = new AuthService();
-    $invalidPasswordUser = [
-        "name" => "John Doe",
-        "email" => "johndoe@example.com",
-        "password" => "short",
-        "confirm_password" => "short"
-    ];
-
-    $authService->register($invalidPasswordUser);
-
-    $repository->clearTable();
-  }
-
-  public function testUserCannotRegisterWithInvalidEmail(): void
-  {
-      $this->expectException(EmailException::class);
-
-      $repository = new UserRepository();
-      $authService = new AuthService();
-      $invalidEmailUser = [
-          "name" => "John Doe",
-          "email" => "invalid-email",
-          "password" => "ValidPass123",
-          "confirm_password" => "ValidPass123"
-      ];
-
-      $authService->register($invalidEmailUser);
-
-    $repository->clearTable();
-  }
-
-  public function testUserCannotRegisterIfUserWithProvidedEmailAlreadyExists(): void
-  {
-      $this->expectException(EmailException::class);
-
-      $repository = new UserRepository();
-      $authService = new AuthService();
-      $existingUser = [
-          "name" => "John Doe",
-          "email" => "existinguser@example.com",
-          "password" => "ValidPass123#%",
-          "confirm_password" => "ValidPass123#%"
-      ];
-
-      // Register user for the first time
-      $authService->register($existingUser);
-
-      // Try to register it for the second
-      $authService->register($existingUser);
-
-    $repository->clearTable();
-  }
-
-  public function testUserCannotEnterNameWithMoreThan255Digits(): void
-  {
-    $this->expectException(ValidatorException::class);
-
-    $repository = new UserRepository();
-    $authService = new AuthService();
-    $existingUser = [
-        "name" => str_repeat('John Doe', 255),
-        "email" => "existinguser@example.com",
-        "password" => "ValidPass123#%",
-        "confirm_password" => "ValidPass123#%"
-    ];
-
-    // Register user for the first time
-    $authService->register($existingUser);
-
-    $repository->clearTable();
+    $this->assertNotEmpty($user);
   }
 }
